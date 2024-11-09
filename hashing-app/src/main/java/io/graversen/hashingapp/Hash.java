203d96c4d9f70e111f7ff5e3a3442f1186453054
Martin Graversen <martin@graversen.io>
2024-11-09 16:46:26 +0100

    :zap: Move latency measurement "around" `HashingService`

--- hashing-app/src/main/java/io/graversen/hashingapp/Hash.java
+++ hashing-app/src/main/java/io/graversen/hashingapp/Hash.java
@@ -3,12 +3,9 @@ package io.graversen.hashingapp;
 import lombok.NonNull;
 import lombok.Value;
 
-import java.time.Duration;
-
 @Value
 public class Hash {
     @NonNull String hash;
     @NonNull HashType type;
     @NonNull Integer rounds;
-    @NonNull Duration latency;
 }
--- hashing-app/src/main/java/io/graversen/hashingapp/HashController.java
+++ hashing-app/src/main/java/io/graversen/hashingapp/HashController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @RestController
 @RequiredArgsConstructor
 public class HashController {
@@ -13,13 +16,15 @@ public class HashController {
 
     @PostMapping("/hash")
     public ResponseEntity<CreateHashResponseDTO> hashString(@RequestBody CreateHashRequestDTO request) {
+        final var startedAt = Instant.now();
         final var hash = hashingService.createHash(request.getText(), request.getType(), request.getRounds());
+        final var latency = Duration.between(startedAt, Instant.now());
 
         final var response = new CreateHashResponseDTO(
                 hash.getHash(),
                 hash.getType(),
                 hash.getRounds(),
-                hash.getLatency()
+                latency
         );
 
         return ResponseEntity.ok(response);
